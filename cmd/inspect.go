eeb0cf6cbbaffb2ce8e7e98e62fc8acc93e6dfc5
Ed Zynda <ezynda3@gmail.com>
2024-03-10 21:26:23 +0300

    Add simple inspect command

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -1,14 +1,55 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Ed Zynda <ezynda3@gmail.com>
 */
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/charmbracelet/huh/spinner"
 	"github.com/spf13/cobra"
 )
+
+type Diamond struct {
+	Chain   string `json:"chain"`
+	Diamond struct {
+		Name string `json:"name"`
+		Abi  []struct {
+			Inputs []struct {
+				InternalType string `json:"internalType"`
+				Name         string `json:"name"`
+				Type         string `json:"type"`
+			} `json:"inputs,omitempty"`
+			StateMutability string `json:"stateMutability,omitempty"`
+			Type            string `json:"type"`
+			Name            string `json:"name,omitempty"`
+		} `json:"abi"`
+		Address string `json:"address"`
+		Facets  []struct {
+			Name string `json:"name"`
+			Abi  []struct {
+				Inputs          []any  `json:"inputs"`
+				Name            string `json:"name"`
+				Type            string `json:"type"`
+				Anonymous       bool   `json:"anonymous,omitempty"`
+				Outputs         []any  `json:"outputs,omitempty"`
+				StateMutability string `json:"stateMutability,omitempty"`
+			} `json:"abi"`
+			Address string `json:"address"`
+		} `json:"facets"`
+	} `json:"diamond"`
+	DiamondAbi []struct {
+		Inputs          []any  `json:"inputs,omitempty"`
+		Name            string `json:"name,omitempty"`
+		Type            string `json:"type"`
+		Anonymous       bool   `json:"anonymous,omitempty"`
+		Outputs         []any  `json:"outputs,omitempty"`
+		StateMutability string `json:"stateMutability,omitempty"`
+	} `json:"diamondAbi"`
+}
 
 // inspectCmd represents the inspect command
 var inspectCmd = &cobra.Command{
@@ -21,7 +62,42 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("inspect called")
+		network, err := cmd.Flags().GetString("network")
+		if err != nil {
+			fmt.Println("Error getting network flag")
+		}
+		address, err := cmd.Flags().GetString("address")
+		if err != nil {
+			fmt.Println("Error getting address flag")
+		}
+
+		if address == "" {
+			fmt.Println("Error: --address flag is required")
+			return
+		}
+
+		var resp *http.Response
+		var jsonData Diamond
+		spinner.New().Title("Fetching Diamond Details...").Action(func() {
+			// Fetch JSON from https://louper.dev/diamond/{address}/json?network={network}
+			resp, err = http.Get("https://louper.dev/diamond/" + address + "/json?network=" + network)
+			if err != nil {
+				fmt.Println("Error fetching JSON from Louper API")
+			}
+			defer resp.Body.Close()
+			rawBody, _ := io.ReadAll(resp.Body)
+			// Marshal JSON into struct
+			json.Unmarshal(rawBody, &jsonData)
+		}).Run()
+
+		fmt.Println("Diamond Name: ", jsonData.Diamond.Name)
+		fmt.Println("Diamond Address: ", jsonData.Diamond.Address)
+
+		for _, facet := range jsonData.Diamond.Facets {
+			fmt.Println("-----------------")
+			fmt.Println("Facet Name: ", facet.Name)
+			fmt.Println("Facet Address: ", facet.Address)
+		}
 	},
 }
 
@@ -37,4 +113,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// inspectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	inspectCmd.Flags().StringP("network", "n", "mainnet", "The network the diamond contract is deployed to")
+	inspectCmd.Flags().StringP("address", "a", "", "The address of the diamond contract to inspect")
 }
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 Ed Zynda ezynda3@gmail.com
+Copyright © 2024 Ed Zynda <ezynda3@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
